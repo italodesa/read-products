ad84166cc364a64ef78229bc223298ef36844059
italodesa <italodesa0101@gmail.com>
2026-07-02 14:58:41 -0300

    feat: add getters and setters in Product class

--- src/entity/Product.java
+++ src/entity/Product.java
@@ -10,4 +10,28 @@ public class Product {
         this.price = price;
         this.quantity = quantity;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
