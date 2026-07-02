2abfb1adde963e0dfeeb0d518c57ae205321b8e0
italodesa <italodesa0101@gmail.com>
2026-07-02 14:56:35 -0300

    feat: add Product class

---
+++ src/entity/Product.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class Product {
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Product(String name, Double price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
