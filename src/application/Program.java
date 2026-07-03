2a47f943a7dd53afc0aa50a5dd495ae4c15d0dcc
italodesa <italodesa0101@gmail.com>
2026-07-03 09:58:34 -0300

    feat: add Program logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,45 @@
 package application;
 
-public class Program {
+import entity.Product;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Program {
+    public static void main(String[] args){
+        Path path = Path.of("sales.csv");
+        List<Product> products = new ArrayList<>();
+
+        try (
+                Scanner sc = new Scanner(path);
+                BufferedWriter bw = new BufferedWriter(
+                        new FileWriter(Path.of("program_out","summary.csv").toFile()))
+        ) {
+
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                String[] split = line.split(",");
+                String name = split[0];
+                Double price = Double.parseDouble(split[1]);
+                Integer quantity = Integer.parseInt(split[2]);
+                Product product = new Product(name,price,quantity);
+                products.add(product);
+            }
+
+
+            for(Product p : products){
+                bw.write(p.toString());
+                bw.newLine();
+            }
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/entity/Product.java
+++ src/entity/Product.java
@@ -34,4 +34,9 @@ public class Product {
     public void setQuantity(Integer quantity) {
         this.quantity = quantity;
     }
+
+    @Override
+    public String toString() {
+        return getName() + "," + getPrice() * getQuantity();
+    }
 }
